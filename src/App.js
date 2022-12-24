a9ff536b55023efbae51cb0daf5d4d738b2cb8a0
egg <eggboissaye@gmail.com>
2022-12-24 15:40:01 -0500

    Created Routes and Authentication with Google

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 const App = () => {
+  const [isAuth, setIsAuth] = useState(false)
+
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login setIsAuth={setIsAuth}/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,24 @@
+import {auth, provider} from "../firebase-config"
+import {signInWithPopup} from "firebase/auth"
+import { useNavigate } from "react-router-dom"
+
+const Login = ({ setIsAuth }) => {
+    let navigate = useNavigate()
+
+    function signInWithGoogle() {
+        signInWithPopup(auth, provider).then((res) => {
+            localStorage.setItem("isAuth", true)
+            setIsAuth(true)
+            navigate("/")
+        })
+    }
+    
+    return (
+        <>
+            <h1>Sign In To Continue</h1>
+            <button onClick={signInWithGoogle}>Sign In With Google</button>
+        </>
+    )
+}
+
+export default Login;
